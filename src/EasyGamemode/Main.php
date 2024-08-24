b47e86c08d41ac2f95a544982bcd41f67aaba2e8
ByEvidencias <izanosky30@gmail.com>
2024-08-24 02:30:15 +0200

    Update Main.php

--- src/EasyGamemode/Main.php
+++ src/EasyGamemode/Main.php
@@ -4,14 +4,16 @@ namespace EasyGamemode;
 
 use pocketmine\plugin\PluginBase;
 use EasyGamemode\commands\GamemodeCommand;
+use EasyGamemode\utils\SingletonTrait;
 
 class Main extends PluginBase {
 
+    use SingletonTrait;
+
     public function onEnable(): void {
+        self::$instance = $this;
         $this->getLogger()->info("EasyGamemode has been enabled. Enjoy it. (:");
-        
-        
-        $this->getServer()->getCommandMap()->register("gm", new GamemodeCommand($this));
+        $this->getServer()->getCommandMap()->register("gm", new GamemodeCommand());
     }
 
     public function onDisable(): void {
